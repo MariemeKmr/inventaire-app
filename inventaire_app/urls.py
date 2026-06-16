4b9b7e01c8507438086deef6f6bfe54f35d10d94
MariemeKmr <Mariemekamara04@gmail.com~>
2026-06-16 23:37:21 +0000

    fix(urls): suppression des namespaces dupliques dans urls.py principal

--- inventaire_app/urls.py
+++ inventaire_app/urls.py
@@ -1,13 +1,11 @@
 ﻿from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('sales/', include('inventaire.urls_sales', namespace='sales')),
     path("admin/", admin.site.urls),
-    path("accounts/", include("inventaire.urls_accounts")),
-    path("", include("inventaire.urls")),
-    path("products/", include("inventaire.urls_products", namespace="products")),
+    path("accounts/", include("inventaire.urls_accounts", namespace="accounts")),
     path("categories/", include("inventaire.urls_categories", namespace="categories")),
-
-]
-
+    path("", include("inventaire.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
